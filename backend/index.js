0b2a5d4231b1e1ac364dd46adf2495aaec3a50c0
hjin.yang <hjin.yang@samsung.com>
2022-06-23 17:52:21 +0900

    Add branch post

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const childProcess = require("child_process");
+const fs = require("fs");
+
 var bodyParser = require("body-parser");
+const { pseudoRandomBytes } = require("crypto");
 const app = express();
 const port = 8883;
 
@@ -9,14 +13,48 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/branch", (req, res) => {
-  let branch = ["돈키호테", "역행자", "백만장자의시크릿"];
-  res.send({ result: branch });
+  childProcess.execSync("cd ../brainversion && git checkout main");
+  const branchCommand = childProcess
+    .execSync("cd ../brainversion && git branch")
+    .toString();
+  let branches = branchCommand.trim().split("\n");
+  let getData = [];
+  for (let line of branches) {
+    if (line.trim() != "* main") {
+      getData.push(line.trim());
+    }
+  }
+  res.send({ result: getData });
 });
 
 app.post("/branch", (req, res) => {
-  let branch = ["돈키호테", "역행자", "백만장자의시크릿"];
   console.log(req.body);
-  res.send({ a: "b" });
+  childProcess.execSync(
+    "cd ../brainversion && git checkout -b " + req.body.branchName
+  );
+
+  var data = {
+    bookName: req.body.bookName,
+    WPP: req.body.WPP,
+    memo: req.body.memo,
+    branchName: req.body.branchName,
+    log: [],
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  fs.writeFileSync(
+    "../brainversion/" + req.body.branchName + ".json",
+    jsonData
+  );
+
+  const pushCommand = childProcess
+    .execSync("git push origin " + req.body.branchName, {
+      cwd: "../brainversion",
+    })
+    .toString();
+  console.log(pushCommand);
+  res.send({ result: { status: 200 } });
 });
 
 app.listen(port, () => {
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,106 +1,217 @@
 import { render } from "@testing-library/react";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Container, Row, Col, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-class Add extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookTitle: "",
-      branchName: "",
-      MPP: 100,
-      memo: "",
-    };
-    this.postData = this.postData.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value, // <- 변경 후
+import { useNavigate } from "react-router-dom";
+
+function Add() {
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    bookTitle: "",
+    branchName: "",
+    MPP: 100,
+    memo: "",
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
     });
   };
-  postData = () => {
+  const postData = () => {
     const url = "http://localhost:8883/branch";
-    let data = {
-      bookTitle: this.state.bookTitle,
-      branchName: this.state.branchName,
-      memo: this.state.memo,
-      MPP: this.state.MPP,
-    };
-    if (data.bookTitle && data.branchName) {
+    const { bookTitle, branchName, MPP, memo } = inputs;
+
+    if (bookTitle && branchName) {
       fetch(url, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, cors, *same-origin
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        body: JSON.stringify(inputs), // body data type must match "Content-Type" header
       })
         .then((response) => response.json()) // parses JSON response into native JavaScript objects
-        .then((data) => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+        .then((data) => {
+          if (data.result.status == 200) {
+            navigate("/gitbook");
+          }
+        }) // JSON-string from `response.json()` call
         .catch((error) => console.error(error));
     }
   };
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <div className="Book-Title">GIT BOOK</div>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Form className="d-grid gap-2">
-            <Form.Group className="mb-3" controlId="formBasicTitle">
-              <Form.Label>Book Title</Form.Label>
-              <Col>
-                <Form.Control
-                  name="bookTitle"
-                  type="text"
-                  placeholder="Book Title"
-                  onChange={this.handleChange}
-                />
-              </Col>
-              <br />
-              <Form.Label>Branch Name</Form.Label>
-              <Col>
-                <Form.Control
-                  name="branchName"
-                  type="text"
-                  placeholder="Branch Name"
-                  onChange={this.handleChange}
-                />
-              </Col>
-              <br />
-              <Form.Label>Words per page</Form.Label>
-              <Col>
-                <Form.Control
-                  name="MPP"
-                  type="text"
-                  placeholder="160"
-                  onChange={this.handleChange}
-                />
-              </Col>
-              <br />
-              <Form.Label>Memo</Form.Label>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className="Book-Title">GIT BOOK</div>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Form className="d-grid gap-2">
+          <Form.Group className="mb-3" controlId="formBasicTitle">
+            <Form.Label>Book Title</Form.Label>
+            <Col>
               <Form.Control
-                name="memo"
-                as="textarea"
-                rows={5}
-                onChange={this.handleChange}
+                name="bookTitle"
+                type="text"
+                placeholder="Book Title"
+                onChange={handleChange}
               />
-            </Form.Group>
-            {/* <Link to={{ pathname: "/gitbook" }} className="d-grid gap-2"> */}
-            <Button className="d-grid gap-2" onClick={this.postData}>
-              Submit
-            </Button>
-            {/* </Link> */}
-          </Form>
-        </Row>
-      </Container>
-    );
-  }
+            </Col>
+            <br />
+            <Form.Label>Branch Name</Form.Label>
+            <Col>
+              <Form.Control
+                name="branchName"
+                type="text"
+                placeholder="Branch Name"
+                onChange={handleChange}
+              />
+            </Col>
+            <br />
+            <Form.Label>Words per page</Form.Label>
+            <Col>
+              <Form.Control
+                name="MPP"
+                type="text"
+                placeholder="160"
+                onChange={handleChange}
+              />
+            </Col>
+            <br />
+            <Form.Label>Memo</Form.Label>
+            <Form.Control
+              name="memo"
+              as="textarea"
+              rows={5}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {/* <Link to={{ pathname: "/gitbook" }} className="d-grid gap-2"> */}
+          <Button className="d-grid gap-2" onClick={postData}>
+            Submit
+          </Button>
+          {/* </Link> */}
+        </Form>
+      </Row>
+    </Container>
+  );
 }
 
 export default Add;
+
+// import { render } from "@testing-library/react";
+// import React, { Component } from "react";
+// import { Button, Container, Row, Col, Form } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+
+// class Add extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       bookTitle: "",
+//       branchName: "",
+//       MPP: 100,
+//       memo: "",
+//     };
+//     this.postData = this.postData.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange = (e) => {
+//     this.setState({
+//       [e.target.name]: e.target.value, // <- 변경 후
+//     });
+//   };
+//   postData = () => {
+//     const url = "http://localhost:8883/branch";
+//     console.log(this.props.history );
+//     this.props.history.push("/gitbook");
+//     let data = {
+//       bookTitle: this.state.bookTitle,
+//       branchName: this.state.branchName,
+//       memo: this.state.memo,
+//       MPP: this.state.MPP,
+//     };
+//     if (data.bookTitle && data.branchName) {
+//       fetch(url, {
+//         method: "POST", // *GET, POST, PUT, DELETE, etc.
+//         mode: "cors", // no-cors, cors, *same-origin
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data), // body data type must match "Content-Type" header
+//       })
+//         .then((response) => response.json()) // parses JSON response into native JavaScript objects
+//         .then((data) => {
+//           if (data.result.status == 200) {
+//           }
+//         }) // JSON-string from `response.json()` call
+//         .catch((error) => console.error(error));
+//     }
+//   };
+//   render() {
+//     return (
+//       <Container>
+//         <Row>
+//           <Col>
+//             <div className="Book-Title">GIT BOOK</div>
+//           </Col>
+//         </Row>
+//         <br />
+//         <Row>
+//           <Form className="d-grid gap-2">
+//             <Form.Group className="mb-3" controlId="formBasicTitle">
+//               <Form.Label>Book Title</Form.Label>
+//               <Col>
+//                 <Form.Control
+//                   name="bookTitle"
+//                   type="text"
+//                   placeholder="Book Title"
+//                   onChange={this.handleChange}
+//                 />
+//               </Col>
+//               <br />
+//               <Form.Label>Branch Name</Form.Label>
+//               <Col>
+//                 <Form.Control
+//                   name="branchName"
+//                   type="text"
+//                   placeholder="Branch Name"
+//                   onChange={this.handleChange}
+//                 />
+//               </Col>
+//               <br />
+//               <Form.Label>Words per page</Form.Label>
+//               <Col>
+//                 <Form.Control
+//                   name="MPP"
+//                   type="text"
+//                   placeholder="160"
+//                   onChange={this.handleChange}
+//                 />
+//               </Col>
+//               <br />
+//               <Form.Label>Memo</Form.Label>
+//               <Form.Control
+//                 name="memo"
+//                 as="textarea"
+//                 rows={5}
+//                 onChange={this.handleChange}
+//               />
+//             </Form.Group>
+//             {/* <Link to={{ pathname: "/gitbook" }} className="d-grid gap-2"> */}
+//             <Button className="d-grid gap-2" onClick={this.postData}>
+//               Submit
+//             </Button>
+//             {/* </Link> */}
+//           </Form>
+//         </Row>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default Add;
